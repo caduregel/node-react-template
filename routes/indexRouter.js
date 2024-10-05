91356504816bf80ca4f2ec62feb3ec67103471d2
liam <liam@nehoray.net>
2024-10-05 17:15:05 +0200

    Bug fix

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("Index"));
+indexRouter.get("/", (req, res) => res.render("index", { message: 'Hello World!' }));
 
 
 module.exports = indexRouter;
