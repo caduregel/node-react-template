755855c43b799b90e3d8ae151ac3e53d384cced2
liam <liam@nehoray.net>
2024-10-07 06:44:35 +0200

    Added form handling basics

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
+const { body, validationResult } = require("express-validator");
 
 // Routes
 const indexRouter = require('./routes/indexRouter')
 const exampleRouter = require('./routes/exampleRoute')
 
 app.use('/', indexRouter)
-app.use('/example', exampleRouter)
 
 // Tell app to listen on PORT
 const PORT = process.env.PORT || 3000;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
+const exampleRouter = require("./exampleRoute");
 
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => res.render("index", { message: 'Hello World!' }));
-
+indexRouter.use('/example', exampleRouter)
 
 module.exports = indexRouter;
